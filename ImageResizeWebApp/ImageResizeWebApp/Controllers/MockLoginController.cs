a6e7ac858843c37687eb7eebc1be647a363549a3
Todor Karamfilov <Todor.Karamfilov@devocean.solutions>
2022-10-20 19:10:24 +0300

    login mock controller

---
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/MockLoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace ImageResizeWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MockLoginController : Controller
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> LoginMock([FromHeader]string email)
+        {
+            return Ok(new JwtSecurityToken(expires: DateTime.Now.AddHours(2), audience:"dreamAudience", issuer: email));
+        }
+    }
+}
